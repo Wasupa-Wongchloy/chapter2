798515fa82b6a4fa6779b956c1f50d2dea0ac28d
OnNaJa <OnNaJa@OnNaJa-PC>
2015-03-12 00:04:21 +0700

    Chapter2

---
+++ src/main/java/com/mycompany/chapter2/ForArrat.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.chapter2;
+
+/**
+ *
+ * @author OnNaJa
+ */
+public class ForArrat {
+    public static void main(String[] args) {
+        int a[] = {25,15,37,64,18};
+        int max = 0;
+        for(int  i=1;i<a.length;i++) {
+            if(a[i]>a[max]){
+                max = i;
+                }
+            System.out.println("The maximum of 25 15 37 64 18 is "+a[max]);
+        }
+    }
+}
+    
+    
+    
+
+
